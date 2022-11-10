96d3c534942d2cd44d06440dbc5f3e3aa9663bf3
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-11-10 15:45:17 +0200

    first_commit

---
+++ src/components/shops/Shop.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Catg from "./Catg"
+import ShopCart from "./ShopCart"
+import "./style.css"
+
+const Shop = ({ addToCart, shopItems }) => {
+  return (
+    <>
+      <section className='shop background'>
+        <div className='container d_flex'>
+          {/*<Catg />*/}
+
+          <div className='contentWidth'>
+
+            <div className='product-content  grid1'>
+              <ShopCart addToCart={addToCart} shopItems={shopItems} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Shop
